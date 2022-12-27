642bf938f657cf0f62ff6cf990f17b328d834689
Katya Fadeeva <katfadmail@gmail.com>
2022-12-27 17:19:31 +0200

    set up pieces...still

--- vanilla/js/index.js
+++ vanilla/js/index.js
@@ -10,6 +10,47 @@ const SQUARE_COLOR = {
     light: '#D1B386',
 }
 
+const PIECES = {  
+    black: {
+        figures: [ './img/pieces/bishop_b.svg',
+                   './img/pieces/king_b.svg',
+                   './img/pieces/knight_b.svg',
+                   './img/pieces/queen_b.svg',
+                   './img/pieces/rook_b.svg', ],
+
+        pawn   :   './img/pieces/pawn_b.svg'
+    },
+
+    white: {
+        figures: [ './img/pieces/bishop_w.svg',
+                   './img/pieces/king_w.svg',
+                   './img/pieces/knight_w.svg',
+                   './img/pieces/queen_w.svg',
+                   './img/pieces/rook_w.svg', ],
+
+        pawn   :   './img/pieces/pawn_w.svg'
+    },
+    
+    white_: {
+        bishop: { src: './img/pieces/bishop_w.svg', coord: ['c1', 'f1'] },
+        king:   { src: './img/pieces/king_w.svg',   coord: ['e1'] },
+        knight: { src: './img/pieces/knight_w.svg', coord: ['b1', 'g1'] },
+        queen:  { src: './img/pieces/queen_w.svg',  coord: ['d1'] },
+        rook:   { src: './img/pieces/rook_w.svg',   coord: ['a1', 'h1'] },
+        pawn:   { src: './img/pieces/pawn_w.svg',   coord: ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'] }
+    },
+    
+    black_: {
+        bishop: { src: './img/pieces/bishop_b.svg', coord: ['c8', 'f'] },
+        king:   { src: './img/pieces/king_b.svg',   coord: ['e8'] },
+        knight: { src: './img/pieces/knight_b.svg', coord: ['b8', 'g8'] },
+        queen:  { src: './img/pieces/queen_b.svg',  coord: ['d8'] },
+        rook:   { src: './img/pieces/rook_b.svg',   coord: ['a8', 'h8'] },
+        pawn:   { src: './img/pieces/pawn_b.svg',   coord: ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'] }
+    },
+    
+}
+
 /*const COORD*/
 
 /**
@@ -38,28 +79,28 @@ function setUpPieces() {
     let x = 0
     let y = 0
     
-    const pieceArr = {
-        figures: [ './img/pieces/bishop.svg',
-                   './img/pieces/king.svg',
-                   './img/pieces/knight.svg',
-                   './img/pieces/queen.svg',
-                   './img/pieces/rook.svg', ],
-        
-        pawn   :   './img/pieces/pawn.svg'
-    }
-    
-    
-    
-    for(let src of pieceArr.figures) {
+    for(let src of PIECES.black.figures) {
         createPiece(src, x, y)
         x += SIDE_OF_SQUARE
     }
     
     x = 0
     for(let i = 0; i < 8; i++) {
-        createPiece(pieceArr.pawn, x, SIDE_OF_SQUARE)
+        createPiece(PIECES.black.pawn, x, SIDE_OF_SQUARE)
         x += SIDE_OF_SQUARE
-        console.log(x, y)
+    }
+    
+    x = 0
+    y = 560
+    for(let src of PIECES.white.figures) {
+        createPiece(src, x, y)
+        x += SIDE_OF_SQUARE
+    }
+    
+    x = 0
+    for(let i = 0; i < 8; i++) {
+        createPiece(PIECES.white.pawn, x, y - SIDE_OF_SQUARE)
+        x += SIDE_OF_SQUARE
     }
     
 }
