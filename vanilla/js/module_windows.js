afd3eb57d649231dd289a6f14210ce3a7faddc67
Katya Fadeeva <katfadmail@gmail.com>
2023-03-21 20:27:31 +0200

    work on menu

--- vanilla/js/module_windows.js
+++ vanilla/js/module_windows.js
@@ -7,24 +7,29 @@
                       -> play_debut       
     
 */
-/*
-{ id : 'guess_debut_result',    next : 'guess_or_play' }
-next : [
-            { id : 'guess_debut', next : [{ id : 'guess_debut_stat_full', next : { id   : 'guess_debut_stat_more' },
-                                                                                   next :  null },
-                                          { id : 'guess_debut_game_list', next : null }, ] },
-            
-            { id : 'play_debut', next: null } ]*/
-const MENU_ROUTE = {
-    guess_or_play : {
-        guess_debut : {},
-        play_debut  : {}
-    }
-}         
-
 let closeBtn = document.querySelectorAll('.close_btn')
 const moduleWindowsArr = Object.values(document.querySelectorAll('.mod_wnd'))
 const moduleWindowsIDs = []
+
+
+
+const GuessDebutStatMore = { id: "guess_debut_stat_more", children: [] }
+const GuessDebutStatFull = { id: "guess_debut_stat_full", children: [GuessDebutStatMore] }
+const GuessDebutGameList = { id: "guess_debut_game_list", children: [] }
+const GuessDebut = { id: "guess_debut", children: [GuessDebutStatFull, GuessDebutGameList]}
+const PlayDebut = {}
+const GuessOrPlay = { id: "guess_or_play", children: [GuessDebut, PlayDebut] }
+const whatDebutIsIt = { id: "what_debut_is_it", children: [GuessOrPlay] }
+
+let startTheGameBtn = document.querySelectorAll('.start_the_game_btn')
+
+startTheGameBtn.forEach((btn) => {
+    btn.addEventListener('click', () => openWin(event))
+})
+
+function openWin(e) {
+    console.log(e.target.parentElement)
+}
 
 moduleWindowsArr.forEach((modWin) =>{
     moduleWindowsIDs.push(modWin.id)   
