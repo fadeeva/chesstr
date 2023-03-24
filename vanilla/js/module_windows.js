559d922cdc29b66dd171c8a7f7c900964d176fb5
Katya Fadeeva <katfadmail@gmail.com>
2023-03-24 22:21:30 +0200

    module windows. uprise of hope

--- vanilla/js/module_windows.js
+++ vanilla/js/module_windows.js
@@ -12,23 +12,41 @@ const moduleWindowsArr = Object.values(document.querySelectorAll('.mod_wnd'))
 const moduleWindowsIDs = []
 
 
-
+/*
 const GuessDebutStatMore = { id: "guess_debut_stat_more", children: [] }
 const GuessDebutStatFull = { id: "guess_debut_stat_full", children: [GuessDebutStatMore] }
 const GuessDebutGameList = { id: "guess_debut_game_list", children: [] }
-const GuessDebut = { id: "guess_debut", children: [GuessDebutStatFull, GuessDebutGameList]}
-const PlayDebut = {}
-const GuessOrPlay = { id: "guess_or_play", children: [GuessDebut, PlayDebut] }
-const whatDebutIsIt = { id: "what_debut_is_it", children: [GuessOrPlay] }
+const GuessDebut         = { id: "guess_debut",           children: [GuessDebutStatFull, GuessDebutGameList]}
+const PlayDebut          = {}
+const GuessOrPlay        = { id: "guess_or_play",         children: [GuessDebut, PlayDebut] }
+const whatDebutIsIt      = { id: "what_debut_is_it",      children: [GuessOrPlay] }
 
-let startTheGameBtn = document.querySelectorAll('.start_the_game_btn')
+const modWin = {
+    what_debut_is_it      : whatDebutIsIt,
+    guess_or_play         : GuessOrPlay,
+    
+    guess_game            : { id: "guess_game",      children: [GuessDebut] },
+    play_game             : { id: "play_game",       children: [PlayDebut] },
+    
+    guess_debut           : GuessDebut,
+    guess_debut_game_list : GuessDebutGameList,
+    guess_debut_stat_full : GuessDebutStatFull,
+    guess_debut_stat_more : GuessDebutStatMore,
+}*/
 
-startTheGameBtn.forEach((btn) => {
-    btn.addEventListener('click', () => openWin(event))
+let nextNode = document.querySelectorAll('.next_node')
+
+nextNode.forEach((node) => {
+    node.addEventListener('click', () => openWin(event))
 })
 
 function openWin(e) {
-    console.log(e.target.parentElement)
+    let parent = e.currentTarget
+    let destination_id = parent.getAttribute("next_node")
+    let element = document.getElementById(destination_id)
+    element.classList.toggle('display_none')
+
+    console.log()
 }
 
 moduleWindowsArr.forEach((modWin) =>{
